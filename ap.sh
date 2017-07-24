5e86a27103602dba1fbbcdec92b25fa2f20de9c7
Bianca Tamayo <hi@biancatamayo.me>
2017-07-24 11:04:19 -0700

    [test] more tests
    FullSemver: 0.1.0-unstable.31
    Branch: development

--- ap.sh
+++ ap.sh
@@ -1,5 +1,30 @@
 #!/usr/bin/env bash
 
+# Ansibuddy: https://github.com/btamayo/ansibuddy
+# MIT License
+
+# Copyright (c) 2017 Bianca Tamayo
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# -------------------------------------------------------------------------------
+
 # "Constants"
 base_folder=$PWD
 inventory_dir_name="inventories"
@@ -395,14 +420,14 @@ debug "DEBUG: Playbook file: $passed_playbook_file_name"
 debug "."
 
 # TODO: Bianca Tamayo (Jul 22, 2017) - Add uppress prompt
-while true; do
-    read -p "Continue? " yn
-    case $yn in
-        [Yy]* ) $playbook_command; break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+# while true; do
+#     read -p "Continue? " yn
+#     case $yn in
+#         [Yy]* ) $playbook_command; break;;
+#         [Nn]* ) exit;;
+#         * ) echo "Please answer yes or no.";;
+#     esac
+# done
 
 
 # End of file
